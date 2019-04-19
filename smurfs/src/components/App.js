43b5a8479354b1c1cf1cb7ba79649fb974d4bd3a
Brad Cantrell <brad777mx@hotmail.com>
2019-04-19 09:51:36 -0700

    smurfs showing up, post new smurf not working

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,85 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import { getSmurfs, addSmurf } from '../actions'
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+    constructor( props ) {
+        super( props )
+
+        this.state = {
+            name: '',
+            age: '',
+            height: ''
+        }
+    }
+
+    componentDidMount() {
+        this.props.getSmurfs()
+    }
+
+    handleChanges = e => {
+        this.setState( {
+            [e.target.name]: e.target.value
+        } )
+    }
+
+    addNewSmurf = e => {
+        e.preventDefault()
+        this.props.addSmurf( this.state )
+        this.setState( { name: '', age: '', height: '' } )
+    }
+
+    render() {
+        console.log(this.props.smurfs);
+        
+        return (
+            <div className="App">
+                <h1>Smurf Village</h1>
+                <div>
+                    {this.props.smurfs.map( ( smurf, index ) => (
+                        <div>
+                            <h3>{smurf.name}</h3>
+                            <p>{smurf.age}</p>
+                            <p>{smurf.height}</p>
+                        </div>
+                    ) )}
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        name='name'
+                        value={this.state.name}
+                        placeholder='enter name...'
+                        onChange={this.handleChanges}
+                    />
+                    <input
+                        type="text"
+                        name='age'
+                        value={this.state.age}
+                        placeholder='enter age...'
+                        onChange={this.handleChanges}
+                    />
+                    <input
+                        type="text"
+                        name='height'
+                        value={this.state.height}
+                        placeholder='enter height...'
+                        onChange={this.handleChanges}
+                    />
+                    <button onClick={this.addNewSmurf}>Add Smurf</button>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = state => ( {
+    smurfs: state.smurfs,
+    error: state.error,
+    fetchingSmurfs: state.fetchingSmurfs,
+    postingSmurf: state.postingSmurf // ???
+} )
+
+export default connect( mapStateToProps, { getSmurfs, addSmurf } )( App )
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 
 const store = createStore(
-  reducer, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(thunk)
+    reducer, applyMiddleware( thunk )
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById( 'root' )
 );
